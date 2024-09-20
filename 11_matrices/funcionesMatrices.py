234049d9c1f202a40a1ba22ff736bece7bb110a4
Andresfp97 <123andresportilla@gmail.com>
2024-09-19 21:22:02 -0500

    se trabajo con matrices

---
+++ 11_matrices/funcionesMatrices.py
@@ -0,0 +1,45 @@
+import  random
+def crearMatriz(fil, col):
+    matriz =[]
+    for f in range(fil):
+        matriz.append([None] * col)
+    return matriz
+
+def mostrarMatriz(mat):
+    for f in range(len(mat)):
+        for c in range(len(mat[f])):
+            print(mat[f] [c], end="\t")
+        print("")
+
+
+def inputMatriz(mat):
+     for f in range(len(mat)):
+        print("fila",f+1)
+        for c in range(len(mat[f])):
+            mat[f] [c] = int(input(f"mat[{f+1}][{c+1}]?"))
+
+        print("-" *10, "\n")
+
+def ramdonMatriz(mat, ini, fin):
+    for f in range(len(mat)):
+        for c in range(len(mat[f])):
+            mat[f] [c] = random.randrange(ini, fin)
+
+def matrizIdentidad(mat):
+    tamf = len(mat)
+    tamc = len(mat [0])
+    if tamf == tamc:
+        for f in range(tamf):
+            for c in range(tamc):
+                if f != c:
+                    mat [f][c]= 0
+                else:
+                    mat[f] [c] = 1
+        return mat
+    else:
+        return None
+
+
+    
+    
+
---
+++ 11_matrices/pruebaMatrices.py
@@ -0,0 +1,11 @@
+from  funcionesMatrices import *
+
+matriz = crearMatriz(2,3)
+
+ramdonMatriz(matriz, 10, 101)
+mostrarMatriz(matriz)
+
+
+matriz2 = crearMatriz(4,4)
+matriz2 = matrizIdentidad(matriz2)
+print(matriz2)
